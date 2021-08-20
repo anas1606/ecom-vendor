60efdf5706b61b53cfbd70f71d9e7c35e31015a8
anas1606 <darshan@eliteevince.in>
2021-08-20 11:20:03 +0530

    swagger

--- src/main/java/com/example/vendor/Application.java
+++ src/main/java/com/example/vendor/Application.java
@@ -3,9 +3,11 @@ package com.example.vendor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan("com.example.commanentity")
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
